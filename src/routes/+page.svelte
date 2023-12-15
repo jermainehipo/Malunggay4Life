5ca0f7090a7d9b9149c23c81959bf0814b880708
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2023-12-15 15:02:48 -0700

    Added dummy bottle image, changed styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,112 @@
         </div>
     </div>
 
-    <div class="bg-primary-500 w-[90rem] h-[46.75rem] p-8">
+    <div class="bg-primary-500 w-[90rem] h-[43rem] px-[4rem] py-8">
         <h2>Benefits</h2>
+        <h3>What makes our Moringa Leaves so amazing.</h3>
+        <div class="grid grid-cols-3 gap-20 pt-8">
+            <!-- Grid Item -->
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Boosts Your Immunity</h4>
+                    <p>With 7 times the vitamin C of oranges, fortify your body's defenses and stay healthy year-round.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Energy and Endurance</h4>
+                    <p>Elevate your energy levels and endurance with Moringa's natural properties, helping you power through your day.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Natural Detoxification</h4>
+                    <p>Cleanse your body naturally with Moringa's detoxifying properties, promoting overall well-being.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Radiant Skin and Hair</h4>
+                    <p>Achieve glowing skin and stimulate hair growth with Moringa's potent combination of 46 antioxidants and essential nutrients.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Power-Packed Protein Source</h4>
+                    <p>Enjoy a protein boost that's twice as potent as yogurt, supporting muscle health and overall vitality.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Joint and Inflammation Support</h4>
+                    <p>Experience relief from inflammation and joint discomfort with Moringa's 36 anti-inflammatory compounds.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Revitalize Your Bones</h4>
+                    <p>Benefit from 4 times the calcium found in milk, ensuring strong and healthy bones for an active lifestyle.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Cognitive Clarity</h4>
+                    <p>Enhance mental clarity and focus, giving your brain the nutrients it needs to perform at its best.</p>
+                </div>
+            </div>
+            <div class="flex gap-2">
+                <div>
+                    <i class="fa-solid fa-check fa-2xl"></i>
+                </div>
+                <div>
+                    <h4>Heart Health</h4>
+                    <p>Support a healthy heart with Moringa's impressive potassium content, three times that of a banana.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="px-[4rem] py-16">
+        <h2>Our Products</h2>
+        <div class="grid grid-cols-4 my-8">
+            <!-- Product Item -->
+            <div class="w-fit">
+                <img alt="Moringa Capsules" src="src/lib/Images/bottle.png" class="w-[12.5rem] h-[12.5rem] border">
+                <p>Moringa Capsules</p>
+                <div class="flex">
+                    <i class="fa-solid fa-star" style="color: #ffd500;"></i>
+                    <i class="fa-solid fa-star" style="color: #ffd500;"></i>
+                    <i class="fa-solid fa-star" style="color: #ffd500;"></i>
+                    <i class="fa-solid fa-star" style="color: #ffd500;"></i>
+                    <i class="fa-solid fa-star" style="color: #ffd500;"></i>
+                </div>
+                <p><b>$19.99 CAD</b></p>
+                <button class="btn bg-primary-500 w-full">Add to Cart</button>
+            </div>
+        </div>
     </div>
 </main>
