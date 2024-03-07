d3a77320e468320bdcdd46b4dd94e0c21a4d18ed
Jermaine Hipolito <jermaine.hipolito@edu.sait.ca>
2024-03-06 21:41:52 -0700

    Edited product cards to have overflow

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -3,15 +3,15 @@
     import { cartItems, addToCart } from "../../cart";
     import { getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
 
+    export let product: Product;
+    
+	// Pop up notification whenever an item is successfully added to cart
     const toastStore = getToastStore();
-
     const t: ToastSettings = {
 	    message: "Added item to cart!",
         timeout: 5000,
     };
 
-    export let product: Product;
-    let image = product.src;
     let cart = get(cartItems); // Get Cart Items
     let cartItemIndex = cart.findIndex((item) => {return item.product.id === product.id })  // Find index that cart item is at
     let cartProduct = cart[cartItemIndex];  // Store product
@@ -33,11 +33,11 @@
     <button class="btn bg-primary-500 absolute right-0 bg-primary-500 rounded-[12rem] pl-[0.65rem] pr-[0.6rem] pt-[0.6rem] w-[2.5rem] h-[2.5rem]" on:click={add}>
         <i class="fa-solid fa-plus fa-xl"></i>
     </button>
-   <div class="mt-2 mr-2"> 
+   <div class="max-w-[9rem] md:max-w-[12.5rem] mt-2 mr-2"> 
         <a href="/products/{product.slug.url}" class="hover:no-underline">
             <img src={product.src} alt={product.alt} class="w-[9rem] h-[9rem] md:w-[12.5rem] md:h-[12.5rem] border rounded-[2rem] rounded-tr-[2rem]" />
-            <p class="hover:underline mb-[-4px] font-medium">{product.name}</p>
-            <subtitle class="hover:underline">{product.description}</subtitle>
+            <p class="hover:underline mb-[-4px] font-medium overflow-hidden truncate">{product.name}</p>
+            <p class="hover:underline subtitle overflow-hidden truncate">{product.description}</p>
             <div class="flex">
                 <!-- (January 20, 2024) star from FontAwesome. https://fontawesome.com/icons/star?f=classic&s=solid -->
                 <i class="fa-solid fa-star" style="color: #ffd500;"></i>
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -19,15 +19,15 @@
 		<i class="fa-solid fa-spinner fa-2xl animate-spin absolute top-1/2"></i>
 	</div>
 {:else}
-<main class="container max-w-[90rem] mb-[7.75rem] px-[1rem] lg:px-[2rem] xl:px-[7.62rem]">
-	<div>
-		<h1>Our Premium Products</h1>
-	</div>
-	<div class="flex flex-wrap gap-[1rem] md:gap-[4rem] lg:gap-[2rem] my-8">
-		<!-- Product Item -->
-		{#each products as product}
-			<ProductCard product={product}/>
-		{/each}
-	</div>
-</main>
+	<main class="container max-w-[90rem] mb-[7.75rem] px-[1rem] lg:px-[2rem] xl:px-[7.62rem]">
+		<div>
+			<h1>Our Premium Products</h1>
+		</div>
+		<div class="flex flex-wrap justify-center sm:justify-normal gap-[1rem] md:gap-[2rem] my-8">
+			<!-- Product Item -->
+			{#each products as product}
+				<ProductCard product={product}/>
+			{/each}
+		</div>
+	</main>
 {/if}
